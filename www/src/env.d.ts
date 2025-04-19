a2d1342a1af5ad39ecc40f68332762c7c14ccc5e
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2025-04-19 19:42:03 +0200

    Request body parsing

--- www/src/env.d.ts
+++ www/src/env.d.ts
@@ -2,5 +2,7 @@ declare namespace App {
     interface Locals {
         req: import('express').Request;
         session: import('express-session').Session & Partial<import('express-session').SessionData>;
+        formData?: FormData;
+        jsonData?: unknown;
     }
 }
--- www/src/middleware.ts
+++ www/src/middleware.ts
@@ -6,6 +6,26 @@ declare module 'express-session' {
     }
 }
 
-export const onRequest = defineMiddleware(async ({ request }, next) => {
+export const onRequest = defineMiddleware(async ({ request, locals }, next) => {
+    const contentType = request.headers.get('Content-Type')?.split(';', 1)[0];
+
+    if (contentType === 'application/x-www-form-urlencoded' || contentType === 'multipart/form-data') {
+        const formData = await request.formData().catch(() => null);
+
+        if (!formData) {
+            return new Response(null, { status: 400 });
+        }
+
+        locals.formData = formData;
+    } else if (contentType === 'application/json') {
+        const jsonData = (await request.json().catch(() => undefined)) as unknown;
+
+        if (jsonData === undefined) {
+            return new Response(null, { status: 400 });
+        }
+
+        locals.jsonData = jsonData;
+    }
+
     return next();
 });
