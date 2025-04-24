0c6be40d239f8f6cfa5f7fcedf93d3111a8a4270
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2025-04-24 15:45:39 +0200

    Exclude bcrypt from bundling

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
         ssr: {
             target: 'node',
             noExternal: true,
-            external: ['express'],
+            external: ['express', 'bcrypt'],
         },
     },
 });
