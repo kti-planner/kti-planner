a486e525074ddbe1e49e64ff20d5b645f541bafa
Mikołaj Trzciński <trzccin@gmail.com>
2025-04-25 23:09:15 +0200

    Add user tests

--- www/src/backend/user.test.ts
+++ www/src/backend/user.test.ts
@@ -1,3 +1,86 @@
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
+import { User } from './user';
 
-test('Example test', () => {});
+const exampleUser = {
+    id: '',
+    name: 'First',
+    email: 'first@test.com',
+    password: 'pass1',
+};
+const secondUser = {
+    id: '',
+    name: 'Second',
+    email: 'second@test.com',
+    password: 'pass2',
+};
+
+function expectUser(user: User | null | undefined, check: typeof exampleUser) {
+    expect(user).toHaveProperty('name', check.name);
+    expect(user).toHaveProperty('email', check.email);
+}
+
+test('Can create users', async () => {
+    const user = await User.create(exampleUser);
+    exampleUser.id = user.id;
+    expectUser(user, exampleUser);
+
+    const user2 = await User.create(secondUser);
+    secondUser.id = user2.id;
+    expectUser(user2, secondUser);
+});
+
+test('Cannot create duplicate users', async () => {
+    await expect(User.create(exampleUser)).rejects.toThrow();
+});
+
+test('Can fetch by id', async () => {
+    const user = await User.fetch(exampleUser.id);
+    expectUser(user, exampleUser);
+});
+
+test('Can fetch by email', async () => {
+    const user = await User.fetchByEmail(exampleUser.email);
+    expectUser(user, exampleUser);
+});
+
+test('Cannot fetch nonexistant', async () => {
+    const user = await User.fetch('a');
+    expect(user).toBeNull();
+    const user2 = await User.fetchByEmail('unknown@test.com');
+    expect(user2).toBeNull();
+});
+
+test('Can fetch multiple', async () => {
+    const users = await User.fetchAll();
+    expect(users).toContainEqual({ name: exampleUser.name, email: exampleUser.email });
+    expect(users).toContainEqual({ name: secondUser.name, email: secondUser.email });
+});
+
+test('Can edit', async () => {
+    let user = await User.fetch(exampleUser.id);
+    expect(user).not.toBeNull();
+    const editSpec = {
+        name: 'e' + exampleUser.name,
+        email: 'e' + exampleUser.email,
+        password: 'e' + exampleUser.password,
+    };
+    await user!.edit(editSpec);
+
+    user = await User.fetch(exampleUser.id);
+    expect(user).toHaveProperty('name', editSpec.name);
+    expect(user).toHaveProperty('email', editSpec.email);
+});
+
+test('Recognises password', async () => {
+    const user = await User.fetch(secondUser.id);
+    expect(user).not.toBeNull();
+    const valid = await user!.checkPassword(secondUser.password);
+    expect(valid).toBe(true);
+});
+
+test('Rejects invalid password', async () => {
+    const user = await User.fetch(secondUser.id);
+    expect(user).not.toBeNull();
+    const valid = await user!.checkPassword(secondUser.password + ' ');
+    expect(valid).toBe(false);
+});
