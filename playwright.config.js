174069b0bf25b322d7bc8dc267fcdf2290dfa7f2
Mikołaj Trzciński <trzccin@gmail.com>
2025-04-29 08:00:56 +0200

    Specify locale in playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
     use: {
         baseURL: 'http://localhost:8080',
         video: process.env.CI ? 'off' : 'retain-on-failure',
+        locale: 'pl-PL',
+        timezoneId: 'Europe/Warsaw',
     },
     reporter: 'line',
     testMatch: 'www/tests/e2e/**/*.spec.ts',
--- www/tests/e2e/index.spec.ts
+++ www/tests/e2e/index.spec.ts
@@ -3,19 +3,12 @@ import { test } from './test-setup';
 
 test('Can switch languages', async ({ page }) => {
     await page.goto('/');
-    const signInText: Record<LangId, string> = {
-        'en': 'Sign in',
-        'pl': 'Zaloguj się',
-    };
-    const defaultLang = await page.evaluate(() => window.langId);
-    const signInPageText = await page.locator('#signin-link').innerText();
-    expect(signInPageText).toBe(signInText[defaultLang as LangId]);
+    await expect(page.locator('#signin-link')).toContainText('Zaloguj się');
+    expect(await page.evaluate(() => window.langId)).toBe('pl');
 
     await page.click('#lang-switch-link');
     await page.waitForURL('/');
 
-    const newLang = await page.evaluate(() => window.langId);
-    expect(newLang).not.toBe(defaultLang);
-    const newSignInPageText = await page.locator('#signin-link').innerText();
-    expect(newSignInPageText).toBe(signInText[newLang as LangId]);
+    await expect(page.locator('#signin-link')).toContainText('Sign in');
+    expect(await page.evaluate(() => window.langId)).toBe('en');
 });
