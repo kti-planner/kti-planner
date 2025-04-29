3da3ec81743198e9f7858860c90ade3009ad0e33
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2025-04-29 22:46:01 +0200

    Stylistic changes

--- www/tests/e2e/test-setup.ts
+++ www/tests/e2e/test-setup.ts
@@ -12,11 +12,13 @@ export const test = base.extend({
         page.on('pageerror', err => {
             throw new Error(`Unhandled page exception: ${err.stack ?? err}`);
         });
+
         page.on('console', msg => {
             if (msg.type() === 'error') {
                 throw new Error(`Console error: ${msg.text()} ${JSON.stringify(msg.location())}`);
             }
         });
+
         await use(page);
     },
 });
