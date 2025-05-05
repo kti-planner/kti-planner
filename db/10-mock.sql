2e91f60d60996994e9a7a91cf7bfd81ea20c9846
Mikołaj Trzciński <trzccin@gmail.com>
2025-05-05 21:13:20 +0200

    Add mock semesters

--- db/10-mock.sql
+++ db/10-mock.sql
@@ -5,3 +5,10 @@ VALUES (
   'admin@admin.com',
   '$2b$10$RRdybUw9Ypxd.HR38l8UGORzd36eE.KbVXMUq1ICwBXnoxjAw8g8S' -- bcrypt hash of word kti
 );
+
+INSERT INTO semesters (id, year, type, start_date, end_date)
+VALUES
+  ( gen_random_uuid(), 2024, 'summer', '2025-02-24', '2025-06-29' ),
+  ( gen_random_uuid(), 2024, 'winter', '2024-10-01', '2025-02-18' ),
+  ( gen_random_uuid(), 2023, 'summer', '2024-02-24', '2024-06-29' ),
+  ( gen_random_uuid(), 2023, 'winter', '2023-10-01', '2024-02-18' );
