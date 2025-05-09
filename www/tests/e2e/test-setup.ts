91729d88c248ff803092db6db107cff7f996d3a2
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2025-05-09 20:38:26 +0200

    Isolate e2e tests

--- www/tests/e2e/test-setup.ts
+++ www/tests/e2e/test-setup.ts
@@ -1,3 +1,6 @@
+import child_process from 'node:child_process';
+import crypto from 'node:crypto';
+import net from 'node:net';
 import { test as base } from '@playwright/test';
 
 declare global {
@@ -6,7 +9,32 @@ declare global {
     }
 }
 
+async function getFreePort(): Promise<number> {
+    return new Promise((resolve, reject) => {
+        const server = net.createServer();
+
+        server.listen(0, () => {
+            const address = server.address();
+
+            if (address === null || typeof address === 'string') {
+                reject(new Error('Failed to get port'));
+                return;
+            }
+
+            server.close(error => resolve(address.port));
+        });
+    });
+}
+
+const projectName = `kti-planner-tests-e2e-${Date.now()}-${crypto.randomInt(1e8)}`;
+const appHostPort = await getFreePort();
+const postgresHostPort = await getFreePort();
+
+process.env.APP_HOST_PORT = appHostPort.toString();
+process.env.POSTGRES_HOST_PORT = postgresHostPort.toString();
+
 export const test = base.extend({
+    baseURL: `http://localhost:${appHostPort}`,
     page: async ({ page }, use) => {
         // Fail tests on exceptions or console errors
         page.on('pageerror', err => {
@@ -21,4 +49,32 @@ export const test = base.extend({
 
         await use(page);
     },
+});
+
+test.beforeEach(async () => {
+    await new Promise<void>((resolve, reject) => {
+        child_process.exec(`docker compose -p ${projectName} up -d --wait`, (error, stdout, stderr) => {
+            if (!error) {
+                resolve();
+                return;
+            }
+
+            reject(error);
+        });
+    });
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+});
+
+test.afterEach(async () => {
+    await new Promise<void>((resolve, reject) => {
+        child_process.exec(`docker compose -p ${projectName} down`, (error, stdout, stderr) => {
+            if (!error) {
+                resolve();
+                return;
+            }
+
+            reject(error);
+        });
+    });
 });
