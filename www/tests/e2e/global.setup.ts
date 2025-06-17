08ef01be7c886eb17bd254e065fdc8a896feb81c
Mikołaj Trzciński <trzccin@gmail.com>
2025-06-17 20:48:02 +0200

    fixup! Make db state reproducible across tests

--- www/tests/e2e/global.setup.ts
+++ www/tests/e2e/global.setup.ts
@@ -1,7 +1,7 @@
 import { test as setup } from '@playwright/test';
 import { dumpDb } from './db-utils';
 
-setup('Reset database to mock data', async () => {
+setup('Save database before running tests', async () => {
     // This will be run once before all tests (all files)
     await dumpDb('setup.sql');
 });
