ff253fdf0c4b98482886f374865fedb487b0a664
Mikołaj Trzciński <trzccin@gmail.com>
2025-08-30 21:05:21 +0200

    Set Playwright timezone

--- playwright.config.js
+++ playwright.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         baseURL: 'http://localhost:8080',
         video: process.env.CI ? 'off' : 'retain-on-failure',
         locale: 'en-US',
+        timezoneId: 'Europe/Warsaw',
     },
     reporter: 'line',
     testMatch: 'www/tests/e2e/**/*.spec.ts',
