a0712ad5ff76f7bfb40f4c710fd56111d03317d1
ajgors <igor.stadnicki@gmail.com>
2025-09-05 21:41:15 +0200

    Add dropdown menu e2e

--- www/tests/e2e/index.spec.ts
+++ www/tests/e2e/index.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'playwright/test';
-import { test } from './fixtures';
+import { loginAsAdmin, loginAsTeacher, test } from './fixtures';
 
 test('Does / redirect to correct page', async ({ page }) => {
     await page.goto('/');
@@ -15,4 +15,54 @@ test('Can switch languages', async ({ page }) => {
 
     await expect(page.getByRole('navigation')).toContainText('Zaloguj się');
     expect(await page.evaluate(() => window.langId)).toBe('pl');
+});
+
+test.describe('Dropdown menu', () => {
+    test('Dropdown menu is visible for logged-in user', async ({ page }) => {
+        await page.goto('/');
+        await loginAsAdmin(page);
+
+        await expect(page.locator('.navbar').getByText('A', { exact: true })).toBeVisible();
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+        await page.locator('.navbar').getByRole('button', { name: 'Sign out' }).click();
+
+        await loginAsTeacher(page);
+
+        await expect(page.locator('.navbar').getByText('BN', { exact: true })).toBeVisible();
+    });
+
+    test('Can open dropdown menu', async ({ page }) => {
+        await page.goto('/');
+        await loginAsAdmin(page);
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+
+        await expect(page.locator('.navbar').getByText('Admin')).toBeVisible();
+        await expect(page.locator('.navbar').getByRole('link', { name: 'Profile' })).toBeVisible();
+        await expect(page.locator('.navbar').getByRole('link', { name: 'Users' })).toBeVisible();
+        await expect(page.locator('.navbar').getByRole('link', { name: 'Classrooms' })).toBeVisible();
+        await expect(page.locator('.navbar').getByRole('button', { name: 'Sign out' })).toBeVisible();
+    });
+
+    test('Can access dropdown menu pages', async ({ page }) => {
+        await page.goto('/');
+        await loginAsAdmin(page);
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+        await page.locator('.navbar').getByRole('link', { name: 'Users' }).click();
+        await expect(page).toHaveURL('/users/');
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+        await page.locator('.navbar').getByRole('link', { name: 'Profile' }).click();
+        await expect(page).toHaveURL('/profile/');
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+        await page.locator('.navbar').getByRole('link', { name: 'Classrooms' }).click();
+        await expect(page).toHaveURL('/classrooms/');
+
+        await page.locator('.navbar').getByText('A', { exact: true }).click();
+        await page.locator('.navbar').getByRole('button', { name: 'Sign out' }).click();
+        await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
+    });
 });
