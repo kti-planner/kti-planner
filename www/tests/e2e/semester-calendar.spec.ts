87ee9c83faa8fe329ecf5c79c1bd818e47358e32
Mikołaj Trzciński <trzccin@gmail.com>
2025-09-19 18:22:48 +0200

    Fix e2e tests

--- www/tests/e2e/semester-calendar.spec.ts
+++ www/tests/e2e/semester-calendar.spec.ts
@@ -3,14 +3,13 @@ import { test } from 'tests/e2e/fixtures';
 
 test('Can access semester calendar', async ({ page }) => {
     await page.goto('/semesters/2025-winter/');
-    await page.getByRole('link', { name: 'Go to calendar' }).click();
 
     await expect(page.getByText('29 Sept – 5 Oct 2025')).toBeVisible();
     await expect(page.getByRole('gridcell', { name: 'Wstęp do sieci komputerowych' })).toBeVisible();
 });
 
 test('Can use subjects filter in semester calendar', async ({ page }) => {
-    await page.goto('/semesters/2025-winter/calendar/');
+    await page.goto('/semesters/2025-winter/');
 
     await expect(page.getByRole('gridcell', { name: 'Wstęp do sieci komputerowych' }).first()).toBeVisible();
     await expect(page.getByRole('gridcell', { name: 'Sieci komputerowe' }).first()).toBeVisible();
@@ -27,7 +26,7 @@ test('Can use subjects filter in semester calendar', async ({ page }) => {
 });
 
 test('Can use classrooms filter in semester calendar', async ({ page }) => {
-    await page.goto('/semesters/2025-winter/calendar/');
+    await page.goto('/semesters/2025-winter/');
 
     await expect(page.getByRole('gridcell', { name: 'EA 204' }).first()).toBeVisible();
     await expect(page.getByRole('gridcell', { name: 'EA 142' }).first()).toBeVisible();
@@ -44,7 +43,7 @@ test('Can use classrooms filter in semester calendar', async ({ page }) => {
 });
 
 test('Can use teachers filter in semester calendar', async ({ page }) => {
-    await page.goto('/semesters/2025-winter/calendar/');
+    await page.goto('/semesters/2025-winter/');
 
     await expect(page.getByRole('gridcell', { name: 'Bogdan Nowak' }).first()).toBeVisible();
     await expect(page.getByRole('gridcell', { name: 'Jan Kowalski' }).first()).toBeVisible();
