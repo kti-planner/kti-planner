bd45573872ead2a5e75e14c3b46497e2cef43138
Mikołaj Trzciński <trzccin@gmail.com>
2025-09-21 13:28:59 +0200

    Add e2e test for editing class from semester calendar

--- www/tests/e2e/semester-calendar.spec.ts
+++ www/tests/e2e/semester-calendar.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'playwright/test';
-import { test } from 'tests/e2e/fixtures';
+import { loginAsTeacher, test } from 'tests/e2e/fixtures';
 
 test('Can access semester calendar', async ({ page }) => {
     await page.goto('/semesters/2025-winter/');
@@ -95,4 +95,19 @@ test('Can view class details', async ({ page }) => {
     await page.getByRole('gridcell', { name: '11:15 - 13:00' }).click();
     await expect(page.getByRole('heading', { name: 'Class details' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Save' })).not.toBeVisible();
+});
+
+test('Can edit class time when logged in', async ({ page }) => {
+    await page.goto('/semesters/2025-winter/');
+    await loginAsTeacher(page);
+
+    await page.getByRole('gridcell', { name: '11:15 - 13:00' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Edit class' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Start date' }).fill('2025-10-01T13:15');
+    await page.getByRole('textbox', { name: 'End date' }).fill('2025-10-01T15:00');
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(page.getByRole('gridcell', { name: '11:15 - 13:00' })).not.toBeVisible();
+    await expect(page.getByRole('gridcell', { name: '13:15 - 15:00' })).toBeVisible();
 });
