2ed0c75d2c4f69aa618edc90e797a1c0427ed5d2
Mikołaj Trzciński <trzccin@gmail.com>
2025-10-08 08:43:09 +0200

    Add Markdown component and display subject description

---
+++ www/src/components/Markdown.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import DOMPurify from 'dompurify';
+import MarkdownIt from 'markdown-it';
+
+const { src } = defineProps<{
+    src: string;
+}>();
+
+const md = new MarkdownIt({
+    html: false,
+    linkify: true,
+    breaks: true,
+});
+
+const sanitizedHtml = computed(() => {
+    const rawHTML = md.render(src);
+    return DOMPurify.sanitize(rawHTML);
+});
+</script>
+
+<template>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div class="markdown" v-html="sanitizedHtml"></div>
+</template>
+
+<style scoped lang="scss">
+.markdown {
+    display: contents;
+
+    & > *:first-child {
+        margin-top: 0;
+    }
+
+    & > *:last-child {
+        margin-bottom: 0;
+    }
+}
+</style>
