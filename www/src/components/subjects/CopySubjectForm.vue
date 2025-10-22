c47a26e2fdf66c90459973052c66bcfff897effb
ajgors <igor.stadnicki@gmail.com>
2025-10-22 22:58:54 +0200

    Add subject copy from previous semester

---
+++ www/src/components/subjects/CopySubjectForm.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { langId } from '@components/frontend/lang';
+import { apiGet, apiPost } from '@components/api';
+import type { SemesterData } from '@components/semesters/types';
+import type { SubjectCopyFromPreviousSemesterApiData, SubjectData } from '@components/subjects/types';
+
+const props = defineProps<{
+    currentSemester: SemesterData;
+}>();
+
+const submitFailed = ref(false);
+
+const semestersData = ref<SemesterData[]>([]);
+const subjectsData = ref<SubjectData[]>([]);
+
+const selectedSemester = ref<SemesterData | undefined>(undefined);
+const selectedSubject = ref<SubjectData | undefined>(undefined);
+
+async function submit() {
+    if (selectedSemester.value === undefined || selectedSubject.value === undefined) {
+        return;
+    }
+
+    const success = await apiPost<boolean>(`/semesters/${props.currentSemester.slug}/api/subject-copy/`, {
+        semesterId: selectedSemester.value.id,
+        subjectId: selectedSubject.value.id,
+    } satisfies SubjectCopyFromPreviousSemesterApiData);
+
+    if (success === undefined) {
+        return;
+    }
+
+    submitFailed.value = !success;
+
+    if (success) {
+        window.location.reload();
+    }
+}
+
+onMounted(async () => {
+    const data = await apiGet<SemesterData[]>('/semesters/api/semesters/');
+    if (data !== undefined) {
+        semestersData.value = data.filter(semester => semester.slug !== props.currentSemester.slug);
+
+        selectedSemester.value =
+            semestersData.value.find(
+                s => s.year === props.currentSemester.year - 1 && s.type === props.currentSemester.type,
+            ) || undefined;
+    }
+});
+
+watch(selectedSemester, async newSemester => {
+    if (newSemester) {
+        selectedSubject.value = undefined;
+        const data = await apiGet<SubjectData[]>(`/semesters/${newSemester.slug}/api/subjects/`);
+        if (data !== undefined) {
+            subjectsData.value = data;
+        }
+    }
+});
+
+const translations = {
+    'en': {
+        'Subject will be copied along with all its:': 'Subject will be copied along with all its:',
+        'Exercises': 'Exercises',
+        'Laboratory groups': 'Laboratory groups',
+        "Calendar events won't be copied.": "Calendar events won't be copied.",
+        'Semester to copy from': 'Semester to copy from',
+        'Summer semester': 'Summer semester',
+        'Winter semester': 'Winter semester',
+        'Select a semester to see available subjects to copy.': 'Select a semester to see available subjects to copy.',
+        'Subject to copy': 'Subject to copy',
+        'Copy': 'Copy',
+        'Subject with this name already exists.': 'Subject with this name already exists.',
+    },
+    'pl': {
+        'Subject will be copied along with all its:': 'Przedmiot zostanie skopiowany wraz ze wszystkimi jego:',
+        'Exercises': 'Ćwiczeniami',
+        'Laboratory groups': 'grupami laboratoryjnymi',
+        "Calendar events won't be copied.": 'Wydarzenia kalendarza nie zostaną skopiowane.',
+        'Semester to copy from': 'Semestr, z którego chcesz skopiować',
+        'Summer semester': 'Semestr letni',
+        'Winter semester': 'Semestr zimowy',
+        'Select a semester to see available subjects to copy.':
+            'Wybierz semestr, aby zobaczyć dostępne przedmioty do skopiowania.',
+        'Subject to copy': 'Przedmiot do skopiowania',
+        'Copy': 'Kopiuj',
+        'Subject with this name already exists.': 'Przedmiot o tej nazwie już istnieje.',
+    },
+};
+
+function translate(text: keyof (typeof translations)[LangId]): string {
+    return translations[langId][text];
+}
+
+const semesterId = crypto.randomUUID();
+const subjectId = crypto.randomUUID();
+</script>
+
+<template>
+    <form class="vstack gap-3 mx-auto" style="max-width: 500px" @submit.prevent="submit">
+        <div>
+            {{ translate('Subject will be copied along with all its:') }}
+            <ul>
+                <li>{{ translate('Exercises') }}</li>
+                <li>{{ translate('Laboratory groups') }}</li>
+            </ul>
+            {{ translate("Calendar events won't be copied.") }}
+        </div>
+
+        <div>
+            <label :for="semesterId" class="form-label">{{ translate('Semester to copy from') }}</label>
+            <select :id="semesterId" v-model="selectedSemester" class="form-select" required>
+                <option v-for="semester in semestersData" :key="semester.slug" :value="semester">
+                    {{
+                        `${
+                            semester.type === 'summer' ? translate('Summer semester') : translate('Winter semester')
+                        } ${semester.year}/${semester.year + 1}`
+                    }}
+                </option>
+            </select>
+        </div>
+
+        <div>
+            <p v-if="!selectedSemester">
+                {{ translate('Select a semester to see available subjects to copy.') }}
+            </p>
+            <p v-else>
+                <label :for="subjectId" class="form-label">{{ translate('Subject to copy') }}</label>
+                <select :id="subjectId" v-model="selectedSubject" class="form-select" required>
+                    <option v-for="subject in subjectsData" :key="subject.slug" :value="subject">
+                        {{ subject.name }}
+                    </option>
+                </select>
+            </p>
+        </div>
+
+        <div class="text-center">
+            <button type="submit" class="btn btn-success">{{ translate('Copy') }}</button>
+        </div>
+
+        <div v-if="submitFailed" class="text-center text-danger">
+            {{ translate('Subject with this name already exists.') }}
+        </div>
+    </form>
+</template>
---
+++ www/src/pages/semesters/[semesterSlug]/api/subject-copy.ts
@@ -0,0 +1,91 @@
+import type { APIRoute } from 'astro';
+import { Classroom } from '@backend/classroom';
+import { Exercise, type ExerciseCreateData } from '@backend/exercise';
+import { LaboratoryGroup, type LaboratoryGroupCreateData } from '@backend/laboratory-group';
+import { Semester } from '@backend/semester';
+import { Subject } from '@backend/subject';
+import { subjectCopyFromPreviousSemesterApiSchema } from '@components/subjects/types';
+
+export const POST: APIRoute = async ({ locals, params }) => {
+    const { jsonData, user } = locals;
+
+    if (!user) {
+        return Response.json(null, { status: 404 });
+    }
+
+    const currentSemester = await Semester.fetchBySlug(params.semesterSlug ?? '');
+
+    if (!currentSemester) {
+        return Response.json(null, { status: 404 });
+    }
+
+    const data = subjectCopyFromPreviousSemesterApiSchema.nullable().catch(null).parse(jsonData);
+
+    if (!data) {
+        return Response.json(null, { status: 400 });
+    }
+
+    const semesterToCopyFrom = await Semester.fetch(data.semesterId);
+
+    if (!semesterToCopyFrom) {
+        return Response.json(null, { status: 404 });
+    }
+
+    const subjectToBeCopied = await Subject.fetch(data.subjectId);
+
+    if (!subjectToBeCopied) {
+        return Response.json(null, { status: 404 });
+    }
+
+    const currentSemesterSubjects = await Subject.fetchAllFromSemester(currentSemester);
+
+    // Copy subject
+    if (currentSemesterSubjects.find(s => s.name.toLowerCase() === subjectToBeCopied.name.toLowerCase())) {
+        return Response.json(false, { status: 200 });
+    }
+
+    const newSubject = await Subject.create({
+        name: subjectToBeCopied.name,
+        semester: currentSemester,
+        teachers: await subjectToBeCopied.getTeachers(),
+        description: subjectToBeCopied.description,
+        moodleCourseId: '', // Course id is not copied
+    });
+
+    // Copy exercises
+    const exercisesToBeCopied = await Exercise.fetchAllFromSubject(subjectToBeCopied);
+    const exercisesCreateData: ExerciseCreateData[] = [];
+    for (const exercise of exercisesToBeCopied) {
+        const classroom = await Classroom.fetch(exercise.classroomId);
+
+        if (!classroom) {
+            return Response.json(null, { status: 400 });
+        }
+
+        const teacher = await exercise.getTeacher();
+
+        exercisesCreateData.push({
+            name: exercise.name,
+            exerciseNumber: exercise.exerciseNumber,
+            subject: newSubject,
+            classroom: classroom,
+            teacher: teacher,
+        });
+    }
+
+    await Promise.all(exercisesCreateData.map(createData => Exercise.create(createData)));
+
+    // Copy laboratory groups
+    const laboratoryGroupsToBeCopied = await LaboratoryGroup.fetchAllFromSubject(subjectToBeCopied);
+    const laboratoryGroupCreateData: LaboratoryGroupCreateData[] = [];
+    for (const group of laboratoryGroupsToBeCopied) {
+        laboratoryGroupCreateData.push({
+            name: group.name,
+            subject: newSubject,
+        });
+    }
+
+    await Promise.all(laboratoryGroupCreateData.map(createData => LaboratoryGroup.create(createData)));
+
+    return Response.json(true, { status: 201 });
+};
