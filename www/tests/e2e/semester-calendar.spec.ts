74e0a64dbdc36b24fe085e07100e1150fdc9c769
Mikołaj Trzciński <trzccin@gmail.com>
2025-10-24 20:59:10 +0200

    Add e2e tests for calendar events

--- www/tests/e2e/semester-calendar.spec.ts
+++ www/tests/e2e/semester-calendar.spec.ts
@@ -156,5 +156,119 @@ test('Can edit class time when logged in', async ({ page }) => {
 
     await expect(page.locator('.calendar-wrapper a').filter({ hasText: '13:15 - 15:00' })).toBeVisible();
 });
-    await expect(page.locator('.calendar-wrapper a').filter({ hasText: '13:15 - 15:00' })).toBeVisible();
+
+test('Can view calendar event details', async ({ page }) => {
+    await page.goto('/semesters/2025-winter/');
+
+    await page
+        .locator('.calendar-wrapper a')
+        .filter({ hasText: '11:15 - 13:00' })
+        .filter({ hasText: 'Koło naukowe' })
+        .click();
+
+    await expect(page.getByRole('heading', { name: 'Event details' })).toBeVisible();
+
+    await expect(page.getByText('Date: 2.10.2025 11:15 - 13:00')).toBeVisible();
+    await expect(page.getByText('Name: Koło naukowe')).toBeVisible();
+    await expect(page.getByText('Teacher: Bogdan Nowak')).toBeVisible();
+    await expect(page.getByText('Classroom: EA 204')).toBeVisible();
+
+    await expect(page.getByRole('button', { name: 'Save' })).not.toBeVisible();
+});
+
+test('Can create single calendar event', async ({ page }) => {
+    await page.goto('/semesters/2025-winter/');
+    await loginAsTeacher(page);
+
+    await page.locator('.fc-timegrid-slot-lane[data-time="14:00:00"]').click();
+
+    await expect(page.getByRole('heading', { name: 'Add event' })).toBeVisible();
+
+    await expect(page.getByRole('textbox', { name: 'Date' })).toHaveValue('2025-10-02');
+    await expect(page.getByRole('textbox', { name: 'Start time' })).toHaveValue('14:00');
+    await expect(page.getByRole('textbox', { name: 'End time' })).toHaveValue('15:00');
+
+    await page.getByRole('textbox', { name: 'Name' }).fill('Test event');
+    await page.getByRole('combobox', { name: 'Classroom' }).selectOption('EA 204');
+
+    await page.getByRole('button', { name: 'Add', exact: true }).click();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '14:00 - 15:00' }).filter({ hasText: 'Test event' }),
+    ).toBeVisible();
+});
+
+test('Can create repeating calendar event', async ({ page }) => {
+    await page.goto('/semesters/2025-winter/');
+    await loginAsTeacher(page);
+
+    await page.locator('.fc-timegrid-slot-lane[data-time="14:00:00"]').click();
+
+    await expect(page.getByRole('heading', { name: 'Add event' })).toBeVisible();
+
+    await expect(page.getByRole('textbox', { name: 'Date' })).toHaveValue('2025-10-02');
+    await expect(page.getByRole('textbox', { name: 'Start time' })).toHaveValue('14:00');
+    await expect(page.getByRole('textbox', { name: 'End time' })).toHaveValue('15:00');
+
+    await page.getByRole('checkbox', { name: 'Repeats' }).check();
+    await page.getByRole('spinbutton', { name: 'Repeats every' }).fill('2');
+    await page.getByRole('combobox', { name: 'Ends' }).selectOption('Before the semester ends');
+    await page.getByRole('spinbutton', { name: 'Repeat amount' }).fill('2');
+    await expect(page.getByRole('textbox', { name: 'Last event' })).toHaveValue('2025-10-16');
+
+    await page.getByRole('textbox', { name: 'Name' }).fill('Test event');
+    await page.getByRole('combobox', { name: 'Classroom' }).selectOption('EA 204');
+
+    await page.getByRole('button', { name: 'Add', exact: true }).click();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '14:00 - 15:00' }).filter({ hasText: 'Test event' }),
+    ).toBeVisible();
+
+    const nextWeekBtn = page.getByRole('button', { name: 'Next week' });
+    await nextWeekBtn.click();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '14:00 - 15:00' }).filter({ hasText: 'Test event' }),
+    ).not.toBeVisible();
+
+    await nextWeekBtn.click();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '14:00 - 15:00' }).filter({ hasText: 'Test event' }),
+    ).toBeVisible();
+});
+
+test('Can edit calendar event when logged in', async ({ page }) => {
+    await page.goto('/semesters/2025-winter/');
+    await loginAsTeacher(page);
+
+    await page
+        .locator('.calendar-wrapper a')
+        .filter({ hasText: '11:15 - 13:00' })
+        .filter({ hasText: 'Koło naukowe' })
+        .click();
+
+    await expect(page.getByRole('heading', { name: 'Edit event' })).toBeVisible();
+
+    await expect(page.getByRole('textbox', { name: 'Date' })).toHaveValue('2025-10-02');
+    await expect(page.getByRole('textbox', { name: 'Start time' })).toHaveValue('11:15');
+    await expect(page.getByRole('textbox', { name: 'End time' })).toHaveValue('13:00');
+    await expect(page.getByRole('textbox', { name: 'Name' })).toHaveValue('Koło naukowe');
+    await expect(page.getByText('Teacher: Bogdan Nowak')).toBeVisible();
+    await expectSelectedOptionText(page.getByRole('combobox', { name: 'Classroom' }), 'EA 204');
+
+    await page.getByRole('textbox', { name: 'Date' }).fill('2025-10-03');
+    await page.getByRole('textbox', { name: 'Start time' }).fill('12:15');
+    await page.getByRole('textbox', { name: 'End time' }).fill('14:00');
+    await page.getByRole('textbox', { name: 'Name' }).fill('Koło naukowe 2');
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '11:15 - 13:00' }).filter({ hasText: 'Koło naukowe' }),
+    ).not.toBeVisible();
+
+    await expect(
+        page.locator('.calendar-wrapper a').filter({ hasText: '12:15 - 14:00' }).filter({ hasText: 'Koło naukowe 2' }),
+    ).toBeVisible();
 });
