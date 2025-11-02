3aa2337753ea0b090dfca19a954566b40dbb2bee
Adam Bucior <35536269+AdamBucior@users.noreply.github.com>
2025-11-02 12:23:12 +0100

    Nitpicks

--- www/tests/e2e/subjects.spec.ts
+++ www/tests/e2e/subjects.spec.ts
@@ -46,7 +46,7 @@ test('Can add new subject and prevent duplicate subject creation', async ({ page
     await page.getByRole('textbox', { name: 'Moodle course ID' }).fill('1472');
     await page.getByRole('textbox', { name: 'Description' }).fill('Subject test description <script>alert(0)</script>');
     await page.getByRole('combobox', { name: 'Teachers' }).selectOption('Jan Kowalski');
-    await page.getByRole('combobox', { name: 'Duration' }).selectOption('105');
+    await page.getByRole('combobox', { name: 'Duration' }).selectOption('105 min');
     await page.getByRole('spinbutton', { name: 'How many weeks are between classes?' }).fill('1');
 
     await page.getByRole('button', { name: 'Add', exact: true }).click();
@@ -118,7 +118,7 @@ test('Can edit subject and prevent duplicate subject', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Moodle course ID' }).fill('15');
     await page.getByRole('textbox', { name: 'Description' }).fill('Test description');
     await page.getByRole('combobox', { name: 'Teachers' }).selectOption('Admin');
-    await page.getByRole('combobox', { name: 'Duration' }).selectOption('165');
+    await page.getByRole('combobox', { name: 'Duration' }).selectOption('165 min');
     await page.getByRole('spinbutton', { name: 'How many weeks are between classes?' }).fill('2');
 
     await page.getByRole('button', { name: 'Save' }).click();
